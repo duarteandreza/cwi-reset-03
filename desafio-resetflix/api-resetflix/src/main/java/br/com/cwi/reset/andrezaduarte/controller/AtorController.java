e5f3dd2959de0fb04bcac4d04a798d40ee843ddc
Andreza Duarte <andrezaraujoduarte@gmail.com>
2021-10-20 08:22:36 -0300

    Criando classe AtorController

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/andrezaduarte/controller/AtorController.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/andrezaduarte/controller/AtorController.java
@@ -1,7 +1,9 @@
 package br.com.cwi.reset.andrezaduarte.controller;
 
 import br.com.cwi.reset.andrezaduarte.FakeDatabase;
+import br.com.cwi.reset.andrezaduarte.model.Ator;
 import br.com.cwi.reset.andrezaduarte.request.AtorRequest;
+import br.com.cwi.reset.andrezaduarte.response.AtorEmAtividade;
 import br.com.cwi.reset.andrezaduarte.service.AtorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,10 +20,28 @@ public class AtorController {
         this.atorService = new AtorService(FakeDatabase.getInstance());
     }
 
-    //demais métodos
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void criarAtor(@RequestBody AtorRequest atorRequest) throws Exception {
         this.atorService.criarAtor(atorRequest);
     }
+
+    @GetMapping("/em_atividade")
+    @ResponseStatus(HttpStatus.OK)
+    public List<AtorEmAtividade> listarAtoresEmAtividade (@RequestParam String filtroNome) throws Exception {
+        return this.atorService.listarAtoresEmAtividade(filtroNome);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Ator consultarAtor(@PathVariable Integer id) throws Exception {
+        return this.consultarAtor(Integer id);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Ator> consultarAtores() throws Exception {
+        return this.atorService.consultarAtores();
+    }
+
 }
