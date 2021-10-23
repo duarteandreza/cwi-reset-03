b703b660c28dd7b28abc6f1a0c43d105dcf0bdc2
Andreza Duarte <andrezaraujoduarte@gmail.com>
2021-10-23 08:18:21 -0300

    Atualizando exercício da aula 3

--- Aula-03/primeiro-projeto-spring/src/main/java/br/com/cwi/reset/primeiroprojetospring/controller/FilmeController.java
+++ Aula-03/primeiro-projeto-spring/src/main/java/br/com/cwi/reset/primeiroprojetospring/controller/FilmeController.java
@@ -4,22 +4,65 @@ import br.com.cwi.reset.primeiroprojetospring.domain.AvaliacaoForaDoPadraoExcept
 import br.com.cwi.reset.primeiroprojetospring.domain.Diretor;
 import br.com.cwi.reset.primeiroprojetospring.domain.Filme;
 import br.com.cwi.reset.primeiroprojetospring.domain.Genero;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/filme")
-
 public class FilmeController {
-    @GetMapping
-    public Filme consultarFilme() throws AvaliacaoForaDoPadraoException {
-        Diretor diretor = new Diretor("João", LocalDate.of(1960,05,25), 50, Genero.MASCULINO);
-        return new Filme ("Filme 1", "Filme de comédia", 120, 2015, 5, diretor);
+
+    private static List<Filme> filmes = new ArrayList<>();
+
+    @PostMapping
+    public ResponseEntity<Filme> cadastrarFilme(@RequestBody Filme filme) {
+        Filme filmeExistente = buscarPorNome(filme.getNome());
+        if (filmeExistente != null) {
+            return ResponseEntity.badRequest().build();
+        }
+        filmes.add(filme);
+        return ResponseEntity.ok(filme);
     }
 
+    @GetMapping
+    public List<Filme> consultarTodos() {
+        return filmes;
+    }
 
+    @GetMapping("/{nome}")
+    public Filme buscarFilmePeloNome(@PathVariable String nome) {
+        return buscarPorNome(nome);
+    }
+
+    @DeleteMapping("/{nome}")
+    public void deletarFilme(@PathVariable String nome) {
+        Filme filme = buscarFilmePeloNome(nome);
+        if (filme != null) {
+            filmes.remove(filme);
+        }
+    }
+
+    @PutMapping
+    public Filme atualizarFilme(@RequestBody Filme filme) {
+        Filme filmeExistente = buscarFilmePeloNome(filme.getNome());
+
+        if (filmeExistente != null) {
+            filmes.remove(filmeExistente);
+            filmes.add(filme);
+            return filme;
+        }
+        return null;
+    }
+
+    private Filme buscarPorNome(String nome) {
+        for (Filme filme : filmes) {
+            if (filme.getNome().equals(nome)) {
+                return filme;
+            }
+        }
+        return null;
+    }
 }
